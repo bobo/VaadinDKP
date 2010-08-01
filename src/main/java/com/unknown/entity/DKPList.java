01baf5e1496e155ccdb183e564fdbbd1cbdc3d25
Rickard Dybeck <alde@alde-laptop.(none)>
2010-08-01 09:15:01 +0200

    blah

--- src/main/java/com/unknown/entity/DKPList.java
+++ src/main/java/com/unknown/entity/DKPList.java
@@ -5,6 +5,10 @@
 package com.unknown.entity;
 
 import com.vaadin.data.Item;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
+import com.vaadin.event.MouseEvents.ClickEvent;
+import com.vaadin.event.MouseEvents.ClickListener;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
@@ -23,25 +27,37 @@ public class DKPList extends Table {
     private CharacherDAO characherDAO;
 
     public DKPList(CharacherDAO characherDAO) {
-	this.characherDAO = characherDAO;
-	addContainerProperty("Name", String.class, "");
-	addContainerProperty("DKP", Double.class, 0);
+        this.characherDAO = characherDAO;
+        addContainerProperty("Name", String.class, "");
+        addContainerProperty("DKP", Double.class, 0);
 
     }
 
     public void printList() {
-	List<User> users = characherDAO.getUsers();
-	Collections.sort(users, new Comparator<User>() {
+        List<User> users = characherDAO.getUsers();
+        Collections.sort(users, new Comparator<User>() {
 
-	    @Override
-	    public int compare(User t, User t1) {
-		return t.getDKP() < t1.getDKP() ? 1 : 0;
-	    }
-	});
-	for (User user : users) {
-	    Item addItem = addItem(user);
-	    addItem.getItemProperty("Name").setValue(user.getUsername());
-	    addItem.getItemProperty("DKP").setValue(user.getDKP());
-	}
+            @Override
+            public int compare(User t, User t1) {
+                return t.getDKP() < t1.getDKP() ? 1 : 0;
+            }
+        });
+        for (final User user : users) {
+            Item addItem = addItem(user);
+            addItem.getItemProperty("Name").setValue(user.getUsername());
+            addItem.getItemProperty("DKP").setValue(user.getDKP());
+            this.addListener(new ItemClickListener() {
+
+                @Override
+                public void itemClick(ItemClickEvent event) {
+                    CharachterInfo info = new CharachterInfo(user);
+                    info.printInfo();
+                    getApplication().getMainWindow().addWindow(info);
+                    info.center();
+                    info.setWidth("400px");
+                    info.setHeight("400px");
+                }
+            });
+        }
     }
 }
